4d8654f170df27b3ec0c81629d2c4cfea0a87437
Leonardo Fragoso <110063706+LeonardoRFragoso@users.noreply.github.com>
2024-05-14 22:14:01 -0300

    adicionando arquivo app do desafio

---
+++ app.py
@@ -0,0 +1,83 @@
+class Contato:
+    def __init__(self, nome, telefone, email, favorito=False):
+        self.nome = nome
+        self.telefone = telefone
+        self.email = email
+        self.favorito = favorito
+
+class Agenda:
+    def __init__(self):
+        self.contatos = []
+
+    def adicionar_contato(self, contato):
+        self.contatos.append(contato)
+
+    def mostrar_contatos(self):
+        for i, contato in enumerate(self.contatos):
+            print(f"{i + 1}. {contato.nome} - {contato.telefone} - {contato.email} - {'Favorito' if contato.favorito else ''}")
+
+    def editar_contato(self, indice, nome, telefone, email):
+        contato = self.contatos[indice]
+        contato.nome = nome
+        contato.telefone = telefone
+        contato.email = email
+
+    def marcar_desmarcar_favorito(self, indice):
+        contato = self.contatos[indice]
+        contato.favorito = not contato.favorito
+
+    def mostrar_favoritos(self):
+        favoritos = [contato for contato in self.contatos if contato.favorito]
+        for i, contato in enumerate(favoritos):
+            print(f"{i + 1}. {contato.nome} - {contato.telefone} - {contato.email}")
+
+    def apagar_contato(self, indice):
+        del self.contatos[indice]
+
+def main():
+    agenda = Agenda()
+    while True:
+        print("\n### Menu ###")
+        print("1. Adicionar contato")
+        print("2. Visualizar lista de contatos")
+        print("3. Editar contato")
+        print("4. Marcar/Desmarcar contato como favorito")
+        print("5. Ver lista de contatos favoritos")
+        print("6. Apagar contato")
+        print("0. Sair")
+
+        escolha = input("Escolha uma opção: ")
+
+        if escolha == "0":
+            break
+        elif escolha == "1":
+            nome = input("Nome: ")
+            telefone = input("Telefone: ")
+            email = input("Email: ")
+            contato = Contato(nome, telefone, email)
+            agenda.adicionar_contato(contato)
+            print("Contato adicionado com sucesso!")
+        elif escolha == "2":
+            agenda.mostrar_contatos()
+        elif escolha == "3":
+            indice = int(input("Índice do contato a ser editado: ")) - 1
+            nome = input("Novo nome: ")
+            telefone = input("Novo telefone: ")
+            email = input("Novo email: ")
+            agenda.editar_contato(indice, nome, telefone, email)
+            print("Contato editado com sucesso!")
+        elif escolha == "4":
+            indice = int(input("Índice do contato a ser marcado/desmarcado como favorito: ")) - 1
+            agenda.marcar_desmarcar_favorito(indice)
+            print("Contato marcado/desmarcado como favorito com sucesso!")
+        elif escolha == "5":
+            agenda.mostrar_favoritos()
+        elif escolha == "6":
+            indice = int(input("Índice do contato a ser apagado: ")) - 1
+            agenda.apagar_contato(indice)
+            print("Contato apagado com sucesso!")
+        else:
+            print("Opção inválida. Por favor, escolha uma opção válida.")
+
+if __name__ == "__main__":
+    main()
